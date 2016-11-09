25938545f3e5156e4783146a8867ebfbf00e8b21
Tamara Slosarek <tamaraslosarek@gmail.com>
2016-11-09 12:11:50 +0100

    Show all results for schools on index page

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,13 @@ var client = require('../models/database')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const query = client.query('SELECT * FROM schools;')
-  const title = query.text
-  res.render('index', { title, query: JSON.stringify(query) })
+  const query = 'SELECT * FROM schools;'
+  client.query(query, function(error, result) {
+    if (error) {
+      res.render('index', { query: 'Error', queryResult: JSON.stringify(error) })
+    }
+    res.render('index', { query, queryResult: JSON.stringify(result) })
+  })
 })
-
-function query(requestString) {
-   return new Promise(function(resolve, reject) {
-       client.query(requestString).then(resolve())
-   })
-}
 
 module.exports = router
