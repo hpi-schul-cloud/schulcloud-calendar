a3e67ccbf26440366da63af0cb583f900f62fc79
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2016-11-18 20:06:34 +0100

    Implement first version of simple iCalendar feed

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,56 @@ var client = require('../models/database')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const query = 'SELECT * FROM schools;'
+  const query = 'SELECT * FROM events;'
   client.query(query, function(error, result) {
     if (error) {
       res.render('index', { query: 'Error', queryResult: JSON.stringify(error) })
+    } else {
+      res.render('index', {query, queryResult: JSON.stringify(result)})
     }
-    res.render('index', { query, queryResult: JSON.stringify(result) })
   })
-})
+});
+
+router.get('/calendar/test', function (req, res) {
+
+    const query = 'SELECT * FROM events ORDER BY id ASC';
+    client.query(query, function (error, result) {
+        if (error) {
+            console.error("Query error");
+        } else {
+            var ical = 'BEGIN:VCALENDAR\n';
+            ical += 'VERSION:2.0\n';
+            ical += 'PRODID:http://schulcloud.org/calendar/test/\n';
+            for (var row_count in result.rows) {
+                var start_date = new Date(result.rows[row_count].start_timestamp);
+                var end_date = new Date(result.rows[row_count].end_timestamp);
+                var created_date = new Date(result.rows[row_count].created_timestamp);
+                var last_modified_date = new Date(result.rows[row_count].last_modified_timestamp);
+                ical += 'BEGIN:VEVENT\n';
+                ical += 'UID:' + result.rows[row_count].id + '@schulcloud.org\n';
+                ical += 'LOCATION:' + result.rows[row_count].location + '\n';
+                ical += 'SUMMARY:' + result.rows[row_count].summary + '\n';
+                ical += 'DESCRIPTION:' + result.rows[row_count].description + '\n';
+                ical += 'DTSTART:' + start_date.toISOString().replace(/([:-]|(\..{3}))/g, '') + '\n';
+                ical += 'DTEND:' + end_date.toISOString().replace(/([:-]|(\..{3}))/g, '') + '\n';
+                ical += 'DTSTAMP:' + created_date.toISOString().replace(/([:-]|(\..{3}))/g, '') + '\n';
+                ical += 'LAST-MODIFIED:' + last_modified_date.toISOString().replace(/([:-]|(\..{3}))/g, '') + '\n';
+                ical += 'END:VEVENT\n';
+            }
+            ical += 'END:VCALENDAR\n';
+
+            var Readable = require('stream').Readable;
+            var s = new Readable();
+            s.push(ical);
+            s.push(null);
+            res.writeHead(200, {
+                'Content-Disposition': 'attachment; filename=calendar.ics',
+                'Content-Type': 'text/calendar',  //application/octet-stream?
+                'Content-Length': ical.length
+            });
+            s.pipe(res);
+        }
+    });
+});
 
 module.exports = router
--- schema.sql
+++ schema.sql
@@ -1,55 +1,15 @@
-DROP TABLE IF EXISTS stufe;
-CREATE TABLE stufe (
-    id    INT PRIMARY KEY NOT NULL,
-    name  INT            NOT NULL
-);
+-- Enable UUID generation
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-DROP TABLE IF EXISTS klasse;
-CREATE TABLE klasse (
-    id    INT PRIMARY KEY NOT NULL,
-    zug   TEXT            NOT NULL,
-    stufe INT REFERENCES stufe(id) NOT NULL
-);
-
-DROP TABLE IF EXISTS nutzer;
-CREATE TABLE nutzer (
-    id        INT PRIMARY KEY NOT NULL,
-    name      TEXT            NOT NULL,
-    vorname   TEXT            NOT NULL,
-    klasse    INT REFERENCES klasse(id) NOT NULL
-);
-
-DROP TABLE IF EXISTS termin;
-CREATE TABLE termin (
-    id        INT PRIMARY KEY NOT NULL,
-    name      TEXT            NOT NULL,
-    start     TEXT            NOT NULL,
-    ende      TEXT            NOT NULL
-);
-
-DROP TABLE IF EXISTS stufe_termin;
-CREATE TABLE stufe_termin (
-    stufen_id INT REFERENCES stufe(id) NOT NULL,
-    termin_id INT REFERENCES termin(id) NOT NULL,
-    PRIMARY KEY (stufen_id, termin_id)
-);
-
-DROP TABLE IF EXISTS klasse_termin;
-CREATE TABLE klasse_termin (
-    klasse_id INT REFERENCES klasse(id) NOT NULL,
-    termin_id INT REFERENCES termin(id) NOT NULL,
-    PRIMARY KEY (klasse_id, termin_id)
-);
-
-DROP TABLE IF EXISTS nutzer_termin;
-CREATE TABLE nutzer_termin (
-    nutzer_id INT REFERENCES nutzer(id) NOT NULL,
-    termin_id INT REFERENCES termin(id) NOT NULL,
-    PRIMARY KEY (nutzer_id, termin_id)
-);
-
-DROP TABLE IF EXISTS schule_termin;
-CREATE TABLE schule_termin (
-    termin_id INT REFERENCES termin(id) NOT NULL,
-    PRIMARY KEY (termin_id)
+DROP TABLE IF EXISTS events;
+CREATE TABLE events (
+  id                      UUID UNIQUE PRIMARY KEY             NOT NULL DEFAULT uuid_generate_v4(),
+  summary                 TEXT                                         DEFAULT 'Neuer Termin',
+  location                TEXT                                         DEFAULT NULL,
+  description             TEXT                                         DEFAULT NULL,
+  start_timestamp         TIMESTAMP WITH TIME ZONE            NOT NULL,
+  end_timestamp           TIMESTAMP WITH TIME ZONE            NOT NULL,
+  reference_id            UUID UNIQUE                         NOT NULL,
+  created_timestamp       TIMESTAMP WITH TIME ZONE                     DEFAULT NOW(),
+  last_modified_timestamp TIMESTAMP WITH TIME ZONE                     DEFAULT now()
 );
