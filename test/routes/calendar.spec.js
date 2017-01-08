9d0b98b7fb14164f0b4eddb17976c5dfa9dbd899
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2017-01-08 22:29:21 +0100

    Fix tests

--- test/routes/calendar.spec.js
+++ test/routes/calendar.spec.js
@@ -20,12 +20,12 @@ describe('routes/calendar', function() {
 
         it('gets a calendar', function(done) {
             // needs to be set because we have to wait for promises
-            this.timeout(4000);
+            this.timeout(10000);
             request(app)
                 .get('/calendar/test')
                 .expect('Content-Disposition', /attachment/)
                 .expect('Content-Type', 'text/calendar')
-                .expect('Content-Length', '686')
+                .expect('Content-Length', '668')
                 .expect(200, done);
         });
 
