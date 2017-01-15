030398870782ad9e02081b4f82a2417243d790c3
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2017-01-15 23:32:17 +0100

    handle null value for res in handleSuccess

--- src/routes/utils/handleSuccess.js
+++ src/routes/utils/handleSuccess.js
@@ -1,6 +1,8 @@
 function handleSuccess(res, results = '') {
-    if (!res.headersSent)
+    if (res && !res.headersSent)
         res.status(201).send("Success: " + JSON.stringify(results));
+    else
+        console.error("res unavailable or headers already sent");
 }
 
 module.exports = handleSuccess;
