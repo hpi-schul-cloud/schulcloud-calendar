b9d18c948aadc11f61677902da0d627b32fad149
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2017-01-23 01:29:56 +0100

    Add NOTIFICATION_SERVICE_BASE_PATH

--- src/http_requests/config.js
+++ src/http_requests/config.js
@@ -3,7 +3,7 @@
  */
 
 const SCHULCLOUD_BASE_PATH = "https://schulcloud-api-mock.herokuapp.com";
-const NOTIFICATION_SERVICE_BASE_PATH = "";
+const NOTIFICATION_SERVICE_BASE_PATH = "https://schul-cloud.org:3030";
 exports.SCHULCLOUD_ALL_USERS_FOR_UUID = SCHULCLOUD_BASE_PATH + "/api/all_users/";
 exports.SCHULCLOUD_ALL_SCOPES_FOR_TOKEN = SCHULCLOUD_BASE_PATH + "/api/all_scopes/";
 exports.NOTIFICATION_SERVICE_NEW_NOTIFICATION = NOTIFICATION_SERVICE_BASE_PATH + "/messages";
