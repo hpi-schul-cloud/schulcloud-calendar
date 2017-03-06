0ccbc77a07afbe3d83b205e8cb7de33950e8a898
Tamara Slosarek <tamaraslosarek@gmail.com>
2017-03-07 00:17:30 +0100

    Refactor GET /events

--- src/services/events/getEvents.js
+++ src/services/events/getEvents.js
@@ -1,19 +1,68 @@
-
-const getEventsForFilter = require('./getEventsForFilter');
 const scopesForToken = require('../scopes/scopesForToken');
-const getEventsPerScope = require('./getEventsPerScope');
+const flatten = require('../../utils/flatten');
+const allAlarmsForEvent = require('../../queries/allAlarmsForEvent');
+const getRepeatExceptionForEvent = require('../../queries/getRepeatExceptionForEvent');
+const getRawEvents = require('../../queries/getRawEvents');
 
 function getEvents(filter, token) {
     return new Promise(function (resolve, reject) {
         if (filter.scopeId || filter.eventId) {
-            getEventsForFilter(filter)
-                .then((events) => { resolve(events); })
-                .catch((error) => { reject(error); });
+            completeEvents(filter)
+                .then(resolve)
+                .catch(reject);
         } else {
             scopesForToken(token)
-                .then((scopes) => { resolve(getEventsPerScope(scopes, filter)); })
-                .catch((error) => { reject(error); });
+                .then((scopes) => { return eventsPerScope(scopes, filter); })
+                .then((events) => { resolve(flatten(events)); })
+                .catch(reject);
         }
+    });
+}
+
+function eventsPerScope(scopes, filter) {
+    return new Promise((resolve, reject) => {
+        Promise.all(scopes.map(({id}) => {
+            filter.scopeId = id;
+            return completeEvents(filter);
+        })).then(resolve).catch(reject);
+    });
+}
+
+function completeEvents(filter) {
+    return new Promise((resolve, reject) => {
+        getRawEvents(filter)
+            .then(appendAlarms)
+            .then(appendExdates)
+            .then(resolve)
+            .catch(reject);
+    });
+}
+
+function appendAlarms(events) {
+    return new Promise((resolve, reject) => {
+        if (events.length === 0) resolve(events);
+        events.forEach((event, index) => {
+            allAlarmsForEvent(event['id'])
+                .then((alarms) => {
+                    event.alarms = alarms;
+                    if (index === events.length - 1) resolve(events);
+                })
+                .catch(reject);
+        });
+    });
+}
+
+function appendExdates(events) {
+    return new Promise((resolve, reject) => {
+        if (events.length === 0) resolve(events);
+        events.forEach((event, index) => {
+            getRepeatExceptionForEvent(event['id'])
+                .then((exdates) => {
+                    event.exdates = exdates;
+                    if (index === events.length - 1) resolve(events);
+                })
+                .catch(reject);
+        });
     });
 }
 
