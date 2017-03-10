53c1b4e49c262245d2e991400163f5c85f3596f5
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2017-03-10 11:04:04 +0100

    Read all config variables from environment

--- src/config.js
+++ src/config.js
@@ -3,31 +3,39 @@
  */
 
 /**
- * Notification Settings
+ * Database settings for production environment
  */
-
-exports.NOTIFICATION_SERVICE_TOKEN = 'service1';
-exports.NOTIFICATION_SCHULCLOUD_ID = '1';
+exports.DB_HOST = process.env.DB_HOST;
+exports.DB_PORT = process.env.DB_PORT || 5432;
+exports.DB_USERNAME = process.env.DB_USERNAME;
+exports.DB_PASSWORD = process.env.DB_PASSWORD;
+exports.DB_DATABASE = process.env.DB_DATABASE;
 
 /**
  * Base Paths
  */
-const SCHULCLOUD_BASE_PATH = 'https://schulcloud-api-mock.herokuapp.com';
-const NOTIFICATION_SERVICE_BASE_PATH = 'https://schul-cloud.org:3030';
+const SCHULCLOUD_BASE_PATH = process.env.SCHULCLOUD_BASE_PATH || 'https://schulcloud-api-mock.herokuapp.com';
+const NOTIFICATION_SERVICE_BASE_PATH = process.env.NOTIFICATION_SERVICE_BASE_PATH || 'https://schul-cloud.org:3030';
 
 /**
  * API Paths
  */
-exports.SCHULCLOUD_ALL_USERS_FOR_UUID = SCHULCLOUD_BASE_PATH + '/api/all_users/';
-exports.SCHULCLOUD_ALL_SCOPES_FOR_TOKEN = SCHULCLOUD_BASE_PATH + '/api/all_scopes/';
-exports.NOTIFICATION_SERVICE_NEW_NOTIFICATION = NOTIFICATION_SERVICE_BASE_PATH + '/messages';
+exports.SCHULCLOUD_ALL_USERS_FOR_UUID = process.env.SCHULCLOUD_ALL_USERS_FOR_UUID || SCHULCLOUD_BASE_PATH + '/api/all_users/';
+exports.SCHULCLOUD_ALL_SCOPES_FOR_TOKEN = process.env. SCHULCLOUD_ALL_SCOPES_FOR_TOKEN || SCHULCLOUD_BASE_PATH + '/api/all_scopes/';
+exports.NOTIFICATION_SERVICE_NEW_NOTIFICATION = process.env.NOTIFICATION_SERVICE_NEW_NOTIFICATION || NOTIFICATION_SERVICE_BASE_PATH + '/messages';
+
+/**
+ * Notification Settings
+ */
+exports.NOTIFICATION_SERVICE_TOKEN = process.env.NOTIFICATION_SERVICE_TOKEN || 'service1';
+exports.NOTIFICATION_SCHULCLOUD_ID = process.env.NOTIFICATION_SCHULCLOUD_ID || '1';
 
 /**
  * CORS
  */
-exports.CORS_ORIGIN = 'https://schulcloud.github.io';
+exports.CORS_ORIGIN = process.env.CORS_ORIGIN || 'https://schulcloud.github.io';
 
 /**
  * Logging
  */
-exports.LOG_LEVEL = 'debug';
+exports.LOG_LEVEL = process.env.LOG_LEVEL || 'debug';
--- src/infrastructure/database.js
+++ src/infrastructure/database.js
@@ -3,14 +3,15 @@ const express = require('express');
 const app = express();
 const types = require('pg').types;
 const INTERVAL_OID = 1186;
+const config = require('../config');
 
 let db;
 if (app.get('env') === 'production') {
-    const db_username = process.env.DB_USERNAME;
-    const db_password = process.env.DB_PASSWORD;
-    const db_host = process.env.DB_HOST;
-    const db_port = process.env.DB_PORT || 5432;
-    const db_database = process.env.DB_DATABASE;
+    const db_username = config.DB_USERNAME;
+    const db_password = config.DB_PASSWORD;
+    const db_host = config.DB_HOST;
+    const db_port = config.DB_PORT;
+    const db_database = config.DB_DATABASE;
 
     if (!db_username || !db_password || !db_host || !db_port || !db_database) {
         throw new Error('expected database credentials but incomplete specified');
