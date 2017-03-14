ef8b6d1472d5f810be6de45e9b1192aff855ad98
Tamara Slosarek <tamaraslosarek@gmail.com>
2017-03-14 22:59:59 +0100

    Insert original subscriptions

---
+++ src/queries/subscriptions/insertOriginalSubscription.js
@@ -0,0 +1,21 @@
+const client = require('../../infrastructure/database');
+const errorMessage = require('../utils/errorMessage');
+
+function insertOriginalSubscription(subscriptionId, scopeId) {
+    return new Promise(function(resolve, reject) {
+        const query = 'INSERT INTO original_subscriptions '
+            + '(subscription_id, scope_id) '
+            + 'VALUES ($1, $2) '
+            + 'RETURNING subscription_id, scope_id';
+        client.query(query, [subscriptionId, scopeId], function (error, result) {
+            if (error) {
+                errorMessage(query, error);
+                reject(error);
+            } else {
+                resolve(result.rows[0]);
+            }
+        });
+    });
+}
+
+module.exports = insertOriginalSubscription;
