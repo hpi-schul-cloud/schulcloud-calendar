2b6b77588c4c546f93fd9654de227f28f1fbc72d
Niklas Hoffmann <niklas.hoffmann@student.hpi.de>
2017-03-19 20:28:15 +0100

    Check if log folder exists and create one if not

--- src/infrastructure/logger.js
+++ src/infrastructure/logger.js
@@ -1,6 +1,9 @@
 const winston = require('winston');
 require('winston-daily-rotate-file');
 const config = require('../config');
+const fs = require('fs');
+
+const logDir = __dirname + '/../../' + 'logs';
 
 const consoleLog = new (winston.transports.Console)({
     level: config.LOG_LEVEL,
@@ -13,15 +16,19 @@ const fileLog = new winston.transports.DailyRotateFile({
     filename: config.LOG_LEVEL + '.log',
     datePattern: 'yyyy-MM-dd.',
     prepend: true,
-    dirname: __dirname + '/../../' + 'logs',
+    dirname: logDir,
     handleExceptions: true,
     humanReadableUnhandledException: true,
     level: config.LOG_LEVEL,
     json: false
 });
 
-const logger = new (winston.Logger)({
-    transports: (process.env.NODE_ENV !== 'test') ? [consoleLog, fileLog] : []
-});
+function checkIfFolderExistsAndReturnLogger() {
+    if (!fs.existsSync(logDir))
+        fs.mkdirSync(logDir);
+    return new (winston.Logger)({
+        transports: (process.env.NODE_ENV !== 'test') ? [consoleLog, fileLog] : []
+    });
+}
 
-module.exports = logger;
+module.exports = checkIfFolderExistsAndReturnLogger();
