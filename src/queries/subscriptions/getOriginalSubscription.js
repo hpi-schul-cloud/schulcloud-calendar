a3acef1bc291c8bd612973de92119131debeff72
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2017-04-24 00:09:41 +0200

    Change columns for querying original subscriptions

--- src/queries/subscriptions/getOriginalSubscription.js
+++ src/queries/subscriptions/getOriginalSubscription.js
@@ -4,7 +4,7 @@ const { allColumns } = require('./constants');
 
 function getOriginalSubscription(param) {
     return new Promise(function (resolve, reject) {
-        const query = `SELECT ${allColumns} FROM original_subscriptions WHERE subscription_id = $1`;
+        const query = `SELECT subscription_id, scope_id FROM original_subscriptions WHERE subscription_id = $1`;
         client.query(query, [ param ], function (error, result) {
             if (error) {
                 errorMessage(query, error);
