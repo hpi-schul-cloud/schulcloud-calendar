380ac4a025b953389482d24e848867fde3993a28
CeEv <cedric@it-4d.de>
2018-07-19 14:05:46 +0200

    Fix data transformation bug by request with subscriptionId param to pass the jsonAPI validator.

--- src/services/subscriptions/getSubscriptions.js
+++ src/services/subscriptions/getSubscriptions.js
@@ -5,12 +5,17 @@ const moveScopeIdToArray = require('../_moveScopeIdToArray');
 function getSubscriptions(filter, scopes) {
     return new Promise(function (resolve, reject) {
         if (filter.scopeId || filter.subscriptionId) {
+		//route is also used for feather get request to a single resource 
             getSubscriptionsFromDb(filter)
+				.then(moveScopeIdToArray)	//added to pass the validateSubscriptionJson 
                 .then(resolve)
                 .catch(reject);
         } else {
+			//route is used for find get request 
             subscriptionsPerScope(scopes, filter)
-                .then((subscriptions) => { resolve(flatten(subscriptions)); })
+                .then((subscriptions) => { 
+					resolve(flatten(subscriptions)); 
+				})
                 .catch(reject);
         }
     });
