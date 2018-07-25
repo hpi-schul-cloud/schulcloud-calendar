ab3befbdb5c4ce2bed369edbb3b2e2fe6cf821d2
CeEv <cedric@it-4d.de>
2018-07-25 11:15:14 +0200

    Modified for npm run script.

--- src/services/dbimporter/start.js
+++ src/services/dbimporter/start.js
@@ -1,7 +1,3 @@
 const subscriptionUrlDbImporter = require('./');
 
-subscriptionUrlDbImporter('http://localhost',3000);
-
-//	console.log('test1>>', subscriptionUrlDbLoader('https://calendars.icloud.com/holidays/de_de.ics',scopeId ));
-	
-	//console.log('test5>>', subscriptionUrlDbImporter('http://it-4d.de/calendar.xml',scopeId ));
+subscriptionUrlDbImporter('http://localhost:3000');
