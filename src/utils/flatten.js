37a9bcec10326374f4b90e89ad23f7547cedb801
CeEv <cedric@it-4d.de>
2018-07-30 15:42:11 +0200

    Clear code.

--- src/utils/flatten.js
+++ src/utils/flatten.js
@@ -12,7 +12,6 @@ function flatten(collections,filter) {
     }, []);
 	
 	if( filter && filter['$offset'] && filter['$limit'] && data && data.length>0){
-		console.log(data);
 		const total=data.length;
 		data=data.slice(filter['$offset'],filter['$offset']+filter['$limit']);
 		data[0].total=total;
