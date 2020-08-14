0e115a484f51dc510aeb446a870027a5b788d727
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2020-08-14 21:27:01 +0200

    Add test and fix test setup

--- test/routes/events.spec.js
+++ test/routes/events.spec.js
@@ -11,13 +11,18 @@ const {
     convertEventToJsonApi,
 } = require('../_testutils/');
 
-// const databaseCleaner = new DatabaseCleaner('postgresql');
-const resolvedServerScopes = createOverlayWithDefaultScopes();
-
 /** helpers */
 const getDate = (minOffset) => new Date(new Date().getTime() + (minOffset * 1000 * 60)).toISOString();
 
-const addTestEvents = ({scopeId = '59cce16281297026d02cde123',  userId = '59898b4a26ffc20c510cfcf0', courseName = 'test'}, optionalData = {}) => {
+/** tests */
+describe('routes/events', () => {
+    const userId = '59898b4a26ffc20c510cfcf0';
+    let resetDB;
+    let clearData;
+    let server;
+    const resolvedServerScopes = createOverlayWithDefaultScopes();
+
+    const addTestEvents = async ({scopeId = '59cce16281297026d02cde123',  userId = '59898b4a26ffc20c510cfcf0', courseName = 'test'}, optionalData = {}) => {
         const needed = {
             courseId: scopeId,
             scopeId,
@@ -25,119 +30,100 @@ const addTestEvents = ({scopeId = '59cce16281297026d02cde123',  userId = '59898b
         };
         const eventData = convertEventToJsonApi({ ...needed, ...optionalData });
 
-        return request(app)
-            .post('/events')
-            .send(eventData)
-            .set('Authorization', userId)
-            .then((res) => {
-                if (res.body.errors) {
-                    if (res.body.errors[0]) {
-                        console.log(res.body.errors[0]);
-                    } else {
-                        console.log(res.body.errors);
-                    }
-                    return
-                }
-                return res.body.data[0]
-            })
-            .catch((err) => {
-                console.log('Can not insert test event', err);
-            });
-}
-
-/** tests */
-describe('routes/events', function() {
-    const userId = '59898b4a26ffc20c510cfcf0';
-    let resetDB;
-    let clearData;
-
-    beforeEach((done) => {
-        nock(SCHULCLOUD_BASE_PATH)
+        await nock(SCHULCLOUD_BASE_PATH)
             .get(uri => uri.includes(SERVER_SCOPES_URI))
             .reply(200, () => {
                 return resolvedServerScopes;
             });
-        done();
+    
+        const result = await request(app)
+            .post('/events')
+            .send(eventData)
+            .set('Authorization', userId);
+
+            console.log('Request:', result.body.data);
+
+            return result.body.data;
+    }
+
+    beforeEach(async () => {
+        await nock(SCHULCLOUD_BASE_PATH)
+            .get(uri => uri.includes(SERVER_SCOPES_URI))
+            .reply(200, () => {
+                return resolvedServerScopes;
+            });
     });
 
-    before((done) => {
-		getClient(true).then((client) => {
-			({ resetDB, clearData } = dbUtils(client));
-			resetDB(done);
-		});    
+    before(async () => {
+        const client = await getClient(true);
+        ({ resetDB, clearData } = dbUtils(client));
+        await resetDB();
+        server = await app.listen(3001);
     });
 
-    after((done) => {
-        clearData(done);
+    after(async () => {
+        await clearData();
+        await server.close();
     });
 
     describe('events', () => {
         describe('POST', () => {
             const scopeId = '59cce16281297026d02cde123';
             const courseName = 'post test';
-            before((done) => {
-                addCourseScope(resolvedServerScopes, scopeId, courseName, true);
-                done();
+            before(async () => {
+                await addCourseScope(resolvedServerScopes, scopeId, courseName, true);
             });
 
-            after((done) => {
+            after(async () => {
                 resolvedServerScopes.data = resolvedServerScopes.data.filter(scope => scope.id !== scopeId);
-                done();
             }); 
 
-            it('create event', (done) => {
+            it('create event', async () => {
                 const eventData = convertEventToJsonApi({
                     courseId: scopeId,
                     scopeId,
                     summary: courseName,
                 });
 
-               request(app)
+               const result = await request(app)
                     .post('/events')
                     .send(eventData)
-                    .set('Authorization', userId)
-                    .then((res) => {
-                        const { data } = res.body;
+                    .set('Authorization', userId);
+                
+                const { data } = result.body;
 
-                        expect(data).to.be.an('array').to.have.lengthOf(1);
+                expect(data).to.be.an('array').to.have.lengthOf(1);
 
-                        const relationships = data[0].relationships['scope-ids'];
+                const relationships = data[0].relationships['scope-ids'];
 
-                        expect(relationships).to.be.an('array').to.have.lengthOf(1);
-                        expect(relationships[0]).to.be.equal(scopeId);
+                expect(relationships).to.be.an('array').to.have.lengthOf(1);
+                expect(relationships[0]).to.be.equal(scopeId);
 
-                        expect(data[0].attributes.summary).to.be.equal(courseName);
-
-                        done();
-                    });
+                expect(data[0].attributes.summary).to.be.equal(courseName);
             });
         });
     
         describe('GET', () => {
-            it('get all for this user', (done) => {
-               request(app)
+            it('get all for this user', async () => {
+               const result = await request(app)
                     .get('/events')
                     .query({
                         all: true,
                     })
-                    .set('Authorization', userId)
-                    .then((res) => {
-                        expect(res.body.data).to.be.an('array').to.have.lengthOf(5);
-                        done();
-                    });
+                    .set('Authorization', userId);
+
+                expect(result.body.data).to.be.an('array').to.have.lengthOf(5);
             });
     
-            it('get one specific event by scope-id for this user', (done) => {
-                request(app)
+            it('get one specific event by scope-id for this user', async () => {
+                const result = await request(app)
                     .get('/events')
                     .query({
-                        'scope-id': '5db838ff8517be0028847d1d',
+                        'scopeId': '5db838ff8517be0028847d1d',
                     })
                     .set('Authorization', userId)
-                    .then((res) => {
-                        expect(res.body.data).to.be.an('array').to.have.lengthOf(1);
-                        done();
-                    });
+
+                expect(result.body.data).to.be.an('array').to.have.lengthOf(1);
             });
         });
     
@@ -145,10 +131,10 @@ describe('routes/events', function() {
             const scopeId = '59cce16281297026d02abc123';
             const courseName = 'find time box test';
             let events;
-            before((done) => {
-                addCourseScope(resolvedServerScopes, scopeId, courseName, true);
-                
-                Promise.all([
+            before(async () => {
+                await addCourseScope(resolvedServerScopes, scopeId, courseName, true);
+
+                events = await Promise.all([
                     addTestEvents({ scopeId }, { startDate: getDate(-30), endDate: getDate(30)}), // touched start
                     addTestEvents({ scopeId }, { startDate: getDate(15), endDate: getDate(45)}), // in time
                     addTestEvents({ scopeId }, { startDate: getDate(30), endDate: getDate(90)}), // touched end
@@ -156,33 +142,26 @@ describe('routes/events', function() {
                     addTestEvents({ scopeId }, { startDate: getDate(-60), endDate: getDate(-30)}), // end before - should not found
                     addTestEvents({ scopeId }, { startDate: getDate(90), endDate: getDate(120)}),// start after - should not found
                     addTestEvents({ scopeId: '59cce16281297026d02xyz999' }, { startDate: getDate(15), endDate: getDate(45)}), // other scope - should not found
-                ]).then((e) => {
-                    events = e;
-                    done();
-                }).catch((err) => {
-                    console.log('Can not insert test events.', err)
-                });
+                ]);
             });
 
-            after((done) => {
+            after(async () => {
                 resolvedServerScopes.data = resolvedServerScopes.data.filter(scope => scope.id !== scopeId);
-                done();
+                await resetDB();
             }); 
 
-            it('find all events that touched by requested time box', (done) => {
-                request(app)
+            it('find all events that touched by requested time box', async () => {
+                const result = await request(app)
                     .get('/events')
                     .query({
-                        all: true,
+                        all: false, // todo false and add time box
+                        from: getDate(0),
+                        until: getDate(60),
+                        // scopeId,
                     })
                     .set('Authorization', userId)
-                    .then((res) => {
-                        expect(res.body.data).to.be.an('array').to.have.lengthOf(5);
-                        done();
-                    }).catch((err) => {
-                        console.log(err);
-                        done();
-                    });
+                
+                expect(result.body.data).to.be.an('array').to.have.lengthOf(4);
             });
         });
     
