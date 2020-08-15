9d742226d22b93224f8376f8f46da7c632f24809
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2020-08-15 10:39:20 +0200

    linter and fix query paramter

--- test/routes/calendar.test.js
+++ test/routes/calendar.test.js
@@ -6,35 +6,35 @@ const fillDatabase = require('../testutils/dbUtils');
 
 describe.skip('routes/calendar', function() {
 
-    beforeEach(function(done) {
-        nock('http://localhost:3030')
-        .get('/api/all_scopes/student1_1')
-        .reply(200, '');
-        fillDatabase(getClient(), done);
-    });
+	beforeEach(function(done) {
+		nock('http://localhost:3030')
+		.get('/api/all_scopes/student1_1')
+		.reply(200, '');
+		fillDatabase(getClient(), done);
+	});
 
-    afterEach(function(done) {
-      //  databaseCleaner.clean(dbClient, done);
-        done();
-    });
+	afterEach(function(done) {
+	  //  databaseCleaner.clean(dbClient, done);
+		done();
+	});
 
-    describe('GET calendar/', function() {
+	describe('GET calendar/', function() {
 
-        it('gets a calendar', function(done) {
-            // needs to be set because we have to wait for promises
-            this.timeout(10000);
-            request(app)
-                .get('/calendar?authorization=student1_1')
-                .expect('Content-Disposition', /attachment/)
-                .expect('Content-Type', 'text/calendar')
-                .expect(hasCalendarEntry)
-                .expect(200, done);
+		it('gets a calendar', function(done) {
+			// needs to be set because we have to wait for promises
+			this.timeout(10000);
+			request(app)
+				.get('/calendar?authorization=student1_1')
+				.expect('Content-Disposition', /attachment/)
+				.expect('Content-Type', 'text/calendar')
+				.expect(hasCalendarEntry)
+				.expect(200, done);
 
-            function hasCalendarEntry(res) {
-                const lines = res.text.split('\n');
-                return lines.length > 5;
-            }
-        });
+			function hasCalendarEntry(res) {
+				const lines = res.text.split('\n');
+				return lines.length > 5;
+			}
+		});
 
-    });
+	});
 });
