c24b6c3bf286e40c95a9ff8ab88254468ed04867
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2020-11-10 13:52:40 +0100

    replace db connector for originalEvents

--- src/queries/original-events/deleteOriginalEvent.js
+++ src/queries/original-events/deleteOriginalEvent.js
@@ -1,18 +1,9 @@
-const getClient = require('../../infrastructure/database');
-const errorMessage = require('../utils/errorMessage');
+const db = require('../../infrastructure/databasePromise');
 
-function deleteOriginalEvent(params) {
-    return new Promise(function (resolve, reject) {
-        const query = 'DELETE FROM original_events WHERE event_id = $1';
-        getClient().query(query, params, function (error, result) {
-            if (error) {
-                errorMessage(query, error);
-                reject(error);
-            } else {
-                resolve(result.rows[0]);
-            }
-        });
-    });
+async function deleteOriginalEvent(params) {
+	const query = 'DELETE FROM original_events WHERE event_id = $1';
+	const result = await db.query(query, params);
+	return result[0];
 }
 
 module.exports = deleteOriginalEvent;
