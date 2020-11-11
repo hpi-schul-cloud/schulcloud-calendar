3b5cbab5e204e65c1b5205d1a6edc919792dc71b
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2020-11-11 07:58:37 +0100

    wip resort query

--- src/queries/events/getRawEvents.js
+++ src/queries/events/getRawEvents.js
@@ -8,47 +8,62 @@ const {
 	DAY_IN_MS,
 } = require('../../config');
 
-const FROM = () => new Date(new Date().getTime() - DAY_IN_MS * SCOPE_DISPLAY_OLD_EVENTS_FROM_LAST_DAYS);
-const UNTIL = () => new Date(new Date().getTime() + DAY_IN_MS * SCOPE_DISPLAY_OLD_EVENTS_UNTIL_DAYS);
+const getDate = (offset) => new Date(new Date().getTime() + offset);
+const formatDate = (date) => {
+	return date.toISOString().replace('T', ' ')+' +'+(date.getTimezoneOffset()/60)*-1;
+}
+const FROM = () => getDate(- DAY_IN_MS * SCOPE_DISPLAY_OLD_EVENTS_FROM_LAST_DAYS);
+const UNTIL = () => getDate(DAY_IN_MS * SCOPE_DISPLAY_OLD_EVENTS_UNTIL_DAYS);
 
 async function getRawEvents(filter, scopes) {
 	const { query, params } = buildQuery(filter, scopes);
 	return db.query(query, params);
 }
 
-function buildQuery(filter, scopes) {
-	let { scopeId, eventId, from, until, all } = filter;
-	let query;
-	let params;
+const isInScope = (scopes = {}, scopeId) => Object.keys(scopes).some((id) => id === scopeId);
 
-	if (!scopeId && !eventId && !scopes) {
-		throw new Error('No scopeId or eventId for event selection given');
-	}
+function buildQuery(filter, scopes) {
+	let { scopeId, eventId, from, until } = filter;
+	let query;
+	let params = [];
+	from = formatDate(from || FROM());
+	until = formatDate(until || UNTIL());
+
+	query = `SELECT ${allColumns} FROM events WHERE scope_id = `;
 
 	if (scopeId) {
-		query = `SELECT ${allColumns} FROM events WHERE scope_id = $1`;
-		params = [ scopeId ];
+		if (isInScope(scopes, scopeId) === false) {
+			throw new Error({
+				message: 'Forbidden',
+				code: 403,
+				title: 'Foridden'
+			});
+		}
+		query += '$1';
+		params.push(scopeId);
+	} else {
+		query += 'ANY($1)';
+		params.push(Object.keys(scopes));
 	}
 
 	if (eventId) {
-		query = `SELECT ${allColumns} FROM events WHERE event_id = $1`;
-		params = [ eventId ];
+		query += ' AND event_id = $2';
+		params.push(eventId);
 	}
 
-	//pass an array of scopes
-	if (scopes) {
-		query = `SELECT ${allColumns} FROM events WHERE scope_id = ANY($1)  `;
-		params = [ Object.keys(scopes) ];
-	}
+	const length = params.length;
+	const fromDate = `$${length}`;
+	const untilDate = `$${length+1}`;
+	// TODO make it shorter
+	const inTime = `(dtstart >= ${fromDate} AND dtend <= ${untilDate})`;
+	const startBeforeAndEndAfter = `(dtstart < ${fromDate} AND dtend > ${untilDate})`;
+	const startBeforeAndInTime = `(dtstart < ${fromDate} AND dtend <= ${untilDate})`;
+	const startAfterAndEndAfter = `(dtstart < ${fromDate} AND dtend > ${untilDate})`;
+	query += ` AND (${inTime} OR ${startBeforeAndEndAfter} OR ${startBeforeAndInTime} OR ${startAfterAndEndAfter})`;
+	params.push(from);
+	params.push(until);
 
-	if (!all) {
-		from = isoDateFormat(from || FROM());
-		until = isoDateFormat(until || UNTIL());
-		query = `${query} AND ((repeat_freq IS NOT NULL AND dtstart <= $2 AND repeat_until >= $3) OR (dtstart < $2 AND dtend > $3) )`;
-		params = [ ...params, until, from];
-	}
-
-	query = `${query} ORDER BY id ASC;`;
+	query += ' ORDER BY id ASC;';
 	return { query, params };
 }
 
