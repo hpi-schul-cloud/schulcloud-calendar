cf39021b341f0d940c20b96fa5a5bfc67eaca366
Thomas Feldtkeller <thomas.feldtkeller@hpi.de>
2021-01-13 15:20:43 +0100

    create DB

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
-PGPASSWORD=$DB_PASSWORD psql -U node -d schulcloud_calendar -a -f schema.sql
+PGPASSWORD=$DB_PASSWORD psql -U node -c "CREATE DATABASE $NAMESPACE"
+PGPASSWORD=$DB_PASSWORD psql -U node -d $NAMESPACE -a -f schema.sql
 
 npm start
