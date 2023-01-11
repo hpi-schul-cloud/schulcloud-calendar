ec50170f6afea000ffd120d6ad67bc61a1f8ed9f
MyNameJeremy <original.jeremy.grotjahn@gmail.com>
2023-01-11 14:04:08 +0100

    Refactor regularDateFormat function using destructuring assignment and substr method for efficient and short code.

--- src/parsers/utils/regularDateFormat.js
+++ src/parsers/utils/regularDateFormat.js
@@ -1,21 +1,13 @@
 function regularDateFormat(date) {
-
-    // YYYYMMDD'T'hhmmss'Z'
-
-    const year = date.slice(0, 4);
-    const month = date.slice(4, 6);
-    const day = date.slice(6, 8);
-
-    const hour = date.slice(9, 11);
-    const minute = date.slice(11, 13);
-    const second = date.slice(13, 15);
-
-    let timezone = date.slice(15, date.size);
-    if (timezone === 'Z') {
-        timezone = 'UTC';
-    }
-
-    return new Date(`${year}/${month-1}/${day} ${hour}:${minute}:${second} ${timezone}`);
+  const [year, month, day, hour, minute, second] = [
+    date.substr(0, 4),
+    parseInt(date.substr(4, 2)) - 1,
+    date.substr(6, 2),
+    date.substr(9, 2),
+    date.substr(11, 2),
+    date.substr(13, 2),
+  ];
+  return new Date(`${year}/${month}/${day} ${hour}:${minute}:${second} UTC`);
 }
 
 module.exports = regularDateFormat;
